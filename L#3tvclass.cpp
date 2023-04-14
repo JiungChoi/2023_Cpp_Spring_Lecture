6e75e5165b4e07638aef7f07218fe131ca295eb3
JiungChoi <81892025+JiungChoi@users.noreply.github.com>
2023-04-14 19:20:36 +0900

    Create L#3tvclass.cpp

---
+++ L#3tvclass.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+class tv
+{
+	bool power;
+	int channel;
+	int volume;
+public :
+	void PowerOn() {
+		power = true;
+	}
+	void PoserOff() {
+		power = false;
+	}
+	void IncChannel() {
+		if (power) {
+			channel = (channel + 1) % 1000;
+		}
+	}
+	void DecChannel() {
+		if (power) {
+			channel = (1000 + (channel - 1)) % 1000;
+		}
+	}
+	void IncVolume() {
+		if (power) {
+			volume = (volume + 1) % 1000;
+		}
+	}
+	void DecVolume() {
+		if (power) {
+			volume = (1000 + (volume - 1)) % 1000;
+		}
+	}
+	void showInfo() {
+		if (power) {
+			cout << "Channel : " << channel << endl;
+			cout << "Volume : " << volume << endl;
+		}
+	}
+
+	void initInfo() {
+		channel = 0;  volume = 0;
+	}
+};
+
+int main() {
+	tv t1, t2, t3;
+	t1.PowerOn(); t1.initInfo();
+	t1.IncChannel(); t1.IncChannel(); t1.IncChannel(); t1.IncChannel(); t1.showInfo();
+	t1.DecChannel(); t1.DecChannel(); t1.showInfo();
+	t1.initInfo(); t1.showInfo();
+	
+
+	return 0;
+}
